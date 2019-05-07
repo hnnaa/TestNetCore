12d8f59708a5910006a22a7d6a1eba97fee2df54
hnnaa <hnnaa@sina.com>
2019-05-07 19:57:42 +0800

    new2

--- AspNetCoreTodo/Controllers/ToDoController.cs
+++ AspNetCoreTodo/Controllers/ToDoController.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using AspNetCoreTodo. Models;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        public IActionResult Index()
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
         {
-            var models=new TodoViewModel();
-            models.Items=new TodoItem[1]{new TodoItem {Title="1",Id=Guid.NewGuid()}};
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+            var models = new TodoViewModel();
+            models.Items = new TodoItem[1] { new TodoItem { Title = "1", Id = Guid.NewGuid() } };
             return View(models);
         }
     }
